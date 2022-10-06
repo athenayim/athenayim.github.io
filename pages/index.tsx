af4f7ca443b41dba1f30d7ad06e62ba7ce0e73e3
Athena Yim <asy20@ic.ac.uk>
2022-10-06 13:48:29 +0100

    Move main styles div to index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
 import LandingPage from "./LandingPage";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <LandingPage />;
+  return (
+    <div className={styles.container}>
+      <LandingPage />
+    </div>
+  );
 };
 
 export default Home;
