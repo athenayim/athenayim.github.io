fce3bbbbac80787105b54f8b6e79f93d03fd6ab1
Athena Yim <asy20@ic.ac.uk>
2022-10-06 13:59:29 +0100

    Create navigation bar component

---
+++ components/NavigationBar.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mantine/core";
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <Button>About Me</Button>
+      <Button>Work Experience</Button>
+    </div>
+  );
+};
+
+export default NavigationBar;
