83b39f316bbc92d5096da55ce9bdf74ff3d04914
Athena Yim <athenayimws@gmail.com>
2025-02-23 12:47:24 +0000

    update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
   },
+  output: "export",
 };
 
 module.exports = nextConfig;
